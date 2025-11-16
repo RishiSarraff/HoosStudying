f7e64b1a96c5a48537b09f9cfb3ca402615c30ca
RishiSarraff <sarraff2004@gmail.com>
2025-11-16 18:35:24 -0500

    Rebuilt Project on a different email(school email)

--- backend/app/database.py
+++ backend/app/database.py
@@ -3,6 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 import os
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
+from google.cloud.sql.connector import Connector
 
 load_dotenv()
 
@@ -11,28 +12,34 @@ instance_id = os.getenv("INSTANCE_ID")
 instance_password = os.getenv("INSTANCE_PASSWORD")
 db_name = os.getenv("DATABASE_NAME")
 instance_connection_name = os.getenv("INSTANCE_CONNECTION_NAME")
-host = os.getenv("HOST")
-port = os.getenv("PORT")
 db_user = os.getenv("DB_USER")
 
 echoVal = os.getenv("ENVIRONMENT", "development") == "development"
 
-connection_string = f"mysql+pymysql://{db_user}:{instance_password}@{host}:{port}/{db_name}"
+connector = Connector()
+
+def getconn():
+    conn = connector.connect(
+        instance_connection_name,  
+        "pymysql",
+        user=db_user,
+        password=instance_password,
+        db=db_name
+    )
+    return conn
 
 engine = create_engine(
-    connection_string,
+    "mysql+pymysql://",
+    creator=getconn,
     pool_pre_ping=True,
     pool_recycle=3600,
-    echo=echoVal  #Turns into false when we change ENVIRONMENT in env to production when deploying
+    echo=echoVal
 )
 
-# Create a Session Factory
 localSession = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-# Base class for models
 Base = declarative_base()
 
-# Dependency to manage a DB Session for FastAPI Routes
 def get_db():
     db = localSession()
     try:
