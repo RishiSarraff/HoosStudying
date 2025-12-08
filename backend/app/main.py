608632490c310c3f87fe96c719e2fb7f1365a717
RishiSarraff <sarraff2004@gmail.com>
2025-12-08 02:53:30 -0500

    reverted some changes back

--- backend/app/main.py
+++ backend/app/main.py
@@ -9,7 +9,12 @@ allowed_origins = os.getenv("ALLOWED_ORIGINS", "http://localhost:5173").split(",
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=allowed_origins,
+    allow_origins=[
+        "http://localhost:5173",
+        "http://localhost:3000",
+        "https://hoosstudying-478421.web.app",
+        "https://hoosstudying-478421.firebaseapp.com",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
